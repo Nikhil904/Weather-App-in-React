e014d54bef35e71538bb4f0356bc11ec00f68d0a
Nikhil <jainnikhil472@gmail.com>
2022-02-10 17:32:49 +0530

    remove extra string

--- src/Service/api.js
+++ src/Service/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const API_KEY = 'e31dd336d0d97fb1aa1965f80456e95d';
 const URL = 'http://api.openweathermap.org/data/2.5/weather';
 
-
 export const getWeather = async(city, country) => {
     const data = await axios.get(`${URL}?q=${city},${country}&appid=${API_KEY}&units=metric`)
     return data;
